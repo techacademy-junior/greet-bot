5b03c9cb6a5ef2173245b5a76e21cb98e5c78c8a
tomoaki-kimura <tkim19750404@gmail.com>
2019-11-12 17:33:41 +0900

    add css

--- app.rb
+++ app.rb
@@ -8,7 +8,15 @@ end
 post '/' do
   input = params['keyword']
   # ここにコードを入力してください。
+  if input == "おはよう"
+  
+  elsif input == "こんにちは"
+  
+  elsif input == "こんばんは"
+  
+  else 
 
+  end
 
   # ここまでコードを入力してね。
   erb :index
